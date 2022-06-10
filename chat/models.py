9085d4aa668baf9786e9ecb0e6845fb783e38fa3
Algebra7 <ahmadmameen7@gmail.com>
2022-06-10 17:37:13 +0100

    edit come comments

--- chat/models.py
+++ chat/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Room(models.Model):
-  name = models.CharField(max_length=20)
-  created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from django.db.models.base import ObjectDoesNotExist
 
 from chat.models import Room
 
@@ -10,8 +11,7 @@ def chat_room(request, room_id):
     try:
         # check if rooom exist
         room = Room.objects.get(pk=room_id)
-        # course = request.user.courses_joined.get(id=course_id)
-    except:
-        # user is not a student of the course or course does not exist
+    except ObjectDoesNotExist:
+        # room does not exist
         return HttpResponseForbidden()
     return render(request, 'chat/room.html', {"room": room})
